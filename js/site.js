ce48d473e015bba01e119ffd054a5904a5af8f17
kli-z <klimazachary@gmail.com>
2017-03-03 22:37:27 -0600

    Lots of JS research. Working on regex evaluation.

--- js/site.js
+++ js/site.js
@@ -4,11 +4,24 @@ $.noConflict();
 (
   function($) {
     $(document).ready(function() {
+    //$('#useradd').addClass('blinking');  Getting blinking text would involve pulling code I didn't make.
       alert("ready");
       
-      $("#signup").on("submit", function(e) {
+
+/* Don't want people turning off JS to get around validation, so I'll add a class that has to exist for the page to
+   be accepted. I don't think I'm at the level where I can prevent people from modifying their JS locally. */
+
+      $('#signup').on('submit', function(e) {
         e.preventDefault(); //don't reload the page
-        alert($("#useradd").val());
+        var val = $('#useradd').val());
+        var regex = '^A-Z0-9amou
+        var isValid = return regex.test(val);
+        if(isValid === true) {
+          
+        }
+        else {
+          
+        }
       })
 
     })
